212f27c073cb75ab4b487b610ad3d0390886bfe1
Iver Medina <ivermedina55@gmail.com>
2023-02-18 15:41:02 -0400

    inicio
    primer commit del repositorio

---
+++ TiposDeDatos.cpp
@@ -0,0 +1,11 @@
+//Tipos de datos básicos en C++
+#include<iostream>
+using namespace std;
+int main(){
+	int numero=15;
+	float flotante=10.45;
+	double mayor=16.3456;
+	char letra='a';
+	cout<<letra;
+	return 0;
+}
---
+++ hola mundo.cpp
@@ -0,0 +1,7 @@
+//Primer programa C++ con saludo
+#include<iostream>
+using namespace std;
+int main() {
+	cout<<"Hola Mundo :D"<<endl;
+	return 0;
+}
